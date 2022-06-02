1917ab23627b3b0158cb137451624571e2b9224c
erno <waeerno@gmail.com>
2022-06-02 16:59:52 +0700

    update modal field

--- resources/views/pengajuan/pimpinan/modal.blade.php
+++ resources/views/pengajuan/pimpinan/modal.blade.php
@@ -9,44 +9,54 @@
                 </button>
             </div>
             <div class="card-body">
+                <div class="col-12">
+                    <h6 class="files-section-title mb-75 text-danger">Data Lansia</h6>
+                </div>
                 <dl class="row">
                     <dt class="col-sm-3">Nama</dt>
                     <dd class="col-sm-9">nama lansia.</dd>
+                    <dt class="col-sm-3">NIK</dt>
+                    <dd class="col-sm-9">1243243434.</dd>
+                    <dt class="col-sm-3">Pendamping</dt>
+                    <dd class="col-sm-9"><div class="badge badge-glow badge-primary">Imam al anwar.</div></dd>
                 </dl>
                 <dl class="row">
-                    <dt class="col-sm-3">Euismod</dt>
-                    <dd class="col-sm-9">Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+                    <dt class="col-sm-3">Alamat</dt>
+                    <dd class="col-sm-9">Jl Kotapinang, perenangan.</dd>
+                    <dt class="col-sm-3">Kecamatan</dt>
+                    <dd class="col-sm-9">Sialangkitang.</dd>
                 </dl>
                 <dl class="row">
-                    <dt></dt>
-                    <dd class="col-sm-9 ml-auto">Donec id elit non mi porta gravida at eget metus.</dd>
+                    <dt class="col-sm-3">Tempat lahir</dt>
+                    <dd class="col-sm-9">kotapinang. 19 januari 1997</dd>
+                    <dt class="col-sm-3">Umur</dt>
+                    <dd class="col-sm-9">43 Tahun.</dd>
+                    <dt class="col-sm-3">Agama</dt>
+                    <dd class="col-sm-9">Islam.</dd>
+
                 </dl>
                 <dl class="row">
-                    <dt class="col-sm-3">Malesuada porta</dt>
-                    <dd class="col-sm-9">Etiam porta sem malesuada magna mollis euismod.</dd>
+                    <dt class="col-sm-3">Pendidikan Terakhir</dt>
+                    <dd class="col-sm-9 ml-auto">SD.</dd>
+                    <dt class="col-sm-3">Pekerjaan</dt>
+                    <dd class="col-sm-9">Wiraswasta.</dd>
+                    <dt class="col-sm-3">Jumlah Penghasilan</dt>
+                    <dd class="col-sm-9">14.000.000.</dd>
                 </dl>
                 <dl class="row">
-                    <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
-                    <dd class="col-sm-9">
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-                        risus.
-                    </dd>
-                </dl>
-                <dl class="row">
-                    <dt class="col-sm-3">Nesting</dt>
-                    <dd class="col-sm-9">
-                        <dl class="row">
-                            <dt class="col-sm-4">Nested definition list</dt>
-                            <dd class="col-sm-8">Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc.</dd>
-                        </dl>
-                    </dd>
+                    <dt class="col-sm-3">Pengajuan Ke</dt>
+                    <dd class="col-sm-9">1.</dd>
+                    <dt class="col-sm-3">Nama Usaha</dt>
+                    <dd class="col-sm-9">Gerobak dorong pecel lele.</dd>
+                    <dt class="col-sm-3">Tanggal Pengajuan</dt>
+                    <dd class="col-sm-9">1997 - 12- 22.</dd>
                 </dl>
 
                 <!-- drives area starts-->
                 <div class="drives">
                     <div class="row">
                         <div class="col-12">
-                            <h6 class="files-section-title mb-75">Berkas Pendukung</h6>
+                            <h6 class="files-section-title mb-75 text-danger">Berkas Pendukung</h6>
                         </div>
 
                         {{-- berkas --}}
@@ -60,7 +70,8 @@
                                         </div>
                                     </div>
                                     <div class="my-1">
-                                        <h5>Nama File</h5>
+                                        <h5>KTP</h5>
+                                        <span>Kartu Tanda Penduduk</span>
                                     </div>
                                 </div>
                             </div>
@@ -78,7 +89,8 @@
                                         </div>
                                     </div>
                                     <div class="my-1">
-                                        <h5>Nama File</h5>
+                                        <h5>KK</h5>
+                                        <span>Kartu Keluarga</span>
                                     </div>
                                 </div>
                             </div>
@@ -96,25 +108,7 @@
                                         </div>
                                     </div>
                                     <div class="my-1">
-                                        <h5>Nama File</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {{-- berkas --}}
-
-                        {{-- berkas --}}
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <div class="card shadow-none border cursor-pointer">
-                                <div class="card-body">
-                                    <div class="d-flex justify-content-between">
-                                        <img src="{{ asset('app-assets/images/icons/pdf.png') }}" alt="nama berkas" height="38">
-                                        <div class="dropdown-items-wrapper">
-                                           <i data-feather='menu'></i>
-                                        </div>
-                                    </div>
-                                    <div class="my-1">
-                                        <h5>Nama File</h5>
+                                        <h5>Surat Keterangan Penghasilan</h5>
                                     </div>
                                 </div>
                             </div>
